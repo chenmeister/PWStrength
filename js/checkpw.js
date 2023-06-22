05d2a282eff969f1d3ccdbeab77e869cbee219ce
chenmeister <sean.chen909@gmail.com>
2023-06-21 18:53:46 -0700

    Fixed bug with Toggle Password button

--- js/checkpw.js
+++ js/checkpw.js
@@ -1,43 +1,58 @@
 function togglePassword() {
-    var passInput = document.getElementById('password');
-    var togglePW = document.getElementById('togglePW');
+  var passInput = document.getElementById('password');
+  var togglePW = document.getElementById('togglePW');
+  var buttonText = togglePW?.textContent.trim();
 
-    (passInput && passInput?.type === "password") ? (passInput.type = "text") : (passInput.type = "password");
-    (togglePW && togglePW?.textContent === "Show Password") ? (togglePW.textContent = "Hide Password") : (togglePW.textContent = "Show Password");
+  passInput && passInput?.type === 'password'
+    ? (passInput.type = 'text')
+    : (passInput.type = 'password');
+  togglePW && buttonText === 'Show Password'
+    ? (togglePW.textContent = 'Hide Password')
+    : (togglePW.textContent = 'Show Password');
 }
 
 function getPassword() {
-    var text = document.getElementById('password')?.value;
+  var text = document.getElementById('password')?.value;
 
-    var length = document.getElementById('length');
-    var lowercase = document.getElementById('lowercase');
-    var uppercase = document.getElementById('uppercase');
-    var number = document.getElementById('number');
-    var special = document.getElementById('special');
+  var length = document.getElementById('length');
+  var lowercase = document.getElementById('lowercase');
+  var uppercase = document.getElementById('uppercase');
+  var number = document.getElementById('number');
+  var special = document.getElementById('special');
 
-    checkIfEightChar(text) ? length?.classList.add('list-group-item-success') : length?.classList.remove('list-group-item-success');
-    checkIfOneLowercase(text) ? lowercase?.classList.add('list-group-item-success') : lowercase?.classList.remove('list-group-item-success');
-    checkIfOneUppercase(text) ? uppercase?.classList.add('list-group-item-success') : uppercase?.classList.remove('list-group-item-success');
-    checkIfOneDigit(text) ? number?.classList.add('list-group-item-success') : number?.classList.remove('list-group-item-success');
-    checkIfOneSpecialChar(text) ? special?.classList.add('list-group-item-success') : special?.classList.remove('list-group-item-success');
+  checkIfEightChar(text)
+    ? length?.classList.add('list-group-item-success')
+    : length?.classList.remove('list-group-item-success');
+  checkIfOneLowercase(text)
+    ? lowercase?.classList.add('list-group-item-success')
+    : lowercase?.classList.remove('list-group-item-success');
+  checkIfOneUppercase(text)
+    ? uppercase?.classList.add('list-group-item-success')
+    : uppercase?.classList.remove('list-group-item-success');
+  checkIfOneDigit(text)
+    ? number?.classList.add('list-group-item-success')
+    : number?.classList.remove('list-group-item-success');
+  checkIfOneSpecialChar(text)
+    ? special?.classList.add('list-group-item-success')
+    : special?.classList.remove('list-group-item-success');
 }
 
-function checkIfEightChar(text){
-    return text?.length >= 8;
+function checkIfEightChar(text) {
+  return text?.length >= 8;
 }
 
 function checkIfOneLowercase(text) {
-    return /[a-z]/.test(text);
+  return /[a-z]/.test(text);
 }
 
 function checkIfOneUppercase(text) {
-    return /[A-Z]/.test(text);
+  return /[A-Z]/.test(text);
 }
 
 function checkIfOneDigit(text) {
-    return /[0-9]/.test(text);
+  return /[0-9]/.test(text);
 }
 
 function checkIfOneSpecialChar(text) {
-    return /[~`!#$%\^&*+=\-\[\]\\';,/{}|\\":<>\?]/g.test(text);
+  return /[~`!#$%\^&*+=\-\[\]\\';,/{}|\\":<>\?]/g.test(text);
 }
